9134a7d5a7b133a5dba89321e14851ad694a6660
Zdeněk Němec <xzdeneknemec@outlook.cz>
2021-11-06 17:01:42 +0100

    edited frames - updated baseframe

--- Main.py
+++ Main.py
@@ -79,19 +79,54 @@ root.mainloop()"""
 
 
 class BaseFrame(Frame):
-    def __init__(self, root, name, height):
+    def __init__(self, root, height):
         super().__init__(root, height=height)
         self.root = root
-        self.title = Label(self, text=name)
+
+        # Labels
+        self.choose_cipher = Label(self, text="Choose cipher").place(x=30, y=50)
+        self.plain_text = Label(self, text="Text").place(x=30, y=90)
+        self.choice = Label(self, text="Save encrypted text to txt file").place(x=30, y=170)
+        self.file_name = Label(self, text="Enter file name - leave blank if 'No' is selected").place(x=30, y=210)
+
+        # Inputs
+        self.plain_text_input = Entry(self)
+        self.plain_text_input.place(x=105, y=90)
+        self.file_name_input = Entry(self)
+        self.file_name_input.place(x=30, y=250)
+
+        # OptionMenus
+        self.CIPHERS = ["Encrypt", "Decrypt"]
+        self.choices = ["No", "Yes"]
+
+        self.variable = StringVar(self)
+        self.variable.set(self.CIPHERS[0])
+
+        self.variable2 = StringVar(self)
+        self.variable2.set(self.choices[0])
+
+        self.dropdown_menu = OptionMenu(self, self.variable, *self.CIPHERS)
+        self.dropdown_menu.place(x=160, y=50)
+
+        self.dropdown_menu2 = OptionMenu(self, self.variable2, *self.choices)
+        self.dropdown_menu2.place(x=229, y=170)
 
 
 class CaesarFrame(BaseFrame):
-    def __init__(self, root, name, height):
-        super().__init__(root, name, height=height)
-        self.shift_label = Label(self, text="Shift: ")
+    def __init__(self, root, height):
+        super().__init__(root, height=height)
+        """self.shift_label = Label(self, text="Shift: ")
         self.shift_entry = Entry(self)
         self.shift_label.pack()
-        self.shift_entry.pack()
+        self.shift_entry.pack()"""
+
+
+def change_scene(frame_class):
+    frame = frame_class
+    frame.grid(row=0, column=0, sticky="ew")
+    frame.configure(bg="#27252c")
+    frame.tkraise()
+    print(frame)
 
 
 class GUI(Tk):
@@ -110,17 +145,10 @@ class GUI(Tk):
         self.menubar.add_cascade(label="Application", menu=self.appmenu)
         self.configure(menu=self.menubar)
 
-        self.ciphers = {"Random cipher": BaseFrame(self, "base", 600), "Caesar cipher": CaesarFrame(self, "caesar", 600)}
-
-        for i, (cipher_name, cipher_frame) in enumerate(self.ciphers.items()):
-            Button(self, text=cipher_name, command=lambda: self.change_scene(cipher_name, cipher_frame)).grid(row=i, column=0)
-            print(cipher_name, cipher_frame)
-
-    def change_scene(self, name, frame_class):
-        frame = frame_class
-        frame.grid(row=0, column=0, sticky="ew")
-        frame.tkraise()
-        print(frame)
+        self.caesar_button = Button(self, text="Caesar cipher", command=lambda: change_scene(
+            CaesarFrame(self, 600))).grid(row=1, column=0)
+        self.random_button = Button(self, text="Random cipher", command=lambda: change_scene(
+            BaseFrame(self, 600))).grid(row=2, column=0)
 
 
 gui = GUI(600, 600, "#27252c", "Encrypt-decrypt application")
