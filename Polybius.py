cbab157f98680d85a4ffd89c57ff2990b86ad61e
Zdeněk Němec <xzdeneknemec@outlook.cz>
2021-11-17 23:54:47 +0100

    added polybius square cipher

---
+++ Polybius.py
@@ -0,0 +1,62 @@
+dictionary = {"a": "11",
+              "b": "12",
+              "c": "13",
+              "d": "14",
+              "e": "15",
+              "f": "21",
+              "g": "22",
+              "h": "23",
+              "i": "24",
+              "j": "25",
+              "k": "31",
+              "l": "32",
+              "m": "33",
+              "n": "34",
+              "o": "35",
+              "p": "41",
+              "q": "42",
+              "r": "43",
+              "s": "44",
+              "t": "45",
+              "u": "51",
+              "v": "52",
+              "w": "w",
+              "x": "53",
+              "y": "54",
+              "z": "55"}
+
+
+def polybius_encrypt(text):
+    encrypted_text = ""
+
+    for x in range(len(text)):
+        if text[x] not in "abcdefghijklmnopqrstuvwxyz":
+            encrypted_text += text[x]
+        else:
+            encrypted_text += dictionary[text[x]]
+
+    return encrypted_text
+
+
+def polybius_decrypt(text):
+    decrypted_text = ""
+    key_list = list(dictionary.keys())
+    value_list = list(dictionary.values())
+    a = 0
+    b = ""
+
+    for character in text:
+
+        if character not in "0123456789":
+            decrypted_text += character
+            print(character, "char")
+
+        else:
+            b += character
+            a += 1
+
+            if ((a % 2) == 0) & (a != 0):
+                decrypted_text += key_list[value_list.index(b)]
+                b = ""
+
+    return decrypted_text
