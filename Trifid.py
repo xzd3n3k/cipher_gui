f8bc06b2ca4d80dfe397b5232a0b51a2ea22891f
Zdeněk Němec <xzdeneknemec@outlook.cz>
2021-11-25 07:03:46 +0100

    edited and fixed trifid encrypt - trifid cipher still under construction

--- Trifid.py
+++ Trifid.py
@@ -1,6 +1,7 @@
-def trifid_encrypt(text):
+def trifid_encrypt(text, period):  # CHECK LETTER BY LETTER EVERY POSITION
     encrypted_text = ""
-    square1 = [["e", "p", "s"], ["d", "u", "c"], ["v", "m", "y"]]
+    text = text.lower()
+    square1 = [["e", "p", "s"], ["d", "u", "c"], ["v", "w", "y"]]
     square2 = [["m", ".", "z"], ["l", "k", "x"], ["n", "b", "t"]]
     square3 = [["f", "g", "o"], ["r", "i", "j"], ["h", "a", "q"]]
     squares = [square1, square2, square3]
@@ -26,8 +27,6 @@ def trifid_encrypt(text):
                     pass
                 listIndex += 1
 
-    print(locations)
-
     upper = []
     middle = []
     lower = []
@@ -36,10 +35,43 @@ def trifid_encrypt(text):
         middle.append(locations[x][1])
         lower.append(locations[x][2])
 
-    print(upper)
-    print(middle)
-    print(lower)
+    lines = [upper, middle, lower]
 
+    i = len(upper) // period
+    j = len(upper) % period
+    encrypted_locations = []
+
+    stop = 0
+    start = 0
+    for o in range(i):
+
+        stop += period
+        for p in range(3):
+
+            for q in range(start, stop):
+                encrypted_locations.append(lines[p][q])
+
+        start += period
+
+    for o in range(3):
+        position = (i*period)
+        for p in range(j):
+            encrypted_locations.append(lines[o][position])
+            position += 1
+
+    pair_count = int(len(encrypted_locations)/3)
+    temp = 0
+    locs_to_encrypt = []
+    for i in range(pair_count):
+        locs = ""
+        for j in range(3):
+            locs += str(encrypted_locations[temp])
+            temp += 1
+        locs_to_encrypt.append(locs)
+
+    print(locs_to_encrypt)
+
+trifid_encrypt("DEFEND THE EAST WALL OF THE CASTLE.", 5)
 
 def trifid_decrypt(text):
     pass
