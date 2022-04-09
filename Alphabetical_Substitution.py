30c51f3b174b3160f3abcdb4f897735a47da5310
Zdeněk Němec <xzdeneknemec@outlook.cz>
2022-04-09 12:53:49 +0200

    alphabetical substitution alphabet and ciphertext alphabet input fixed (error msg added to prevend user entering wrong alphabets)

--- Alphabetical_Substitution.py
+++ Alphabetical_Substitution.py
@@ -23,9 +23,19 @@ def alphabetical_substitution_decrypt(text, alphabet="abcdefghijklmnopqrstuvwxyz
 
     if alphabet == "":
         alphabet = "abcdefghijklmnopqrstuvwxyz"
+    else:
+        control_alp = "abcdefghijklmnopqrstuvwxyz"
+        for letter in control_alp:
+            if alphabet.count(letter) != 1:
+                return "Wrong alphabet, some characters are special, missing or used multiple times!"
 
     if cipher_alphabet == "":
         cipher_alphabet = "zyxwvutsrqponmlkjihgfedcba"
+    else:
+        control_alp = "zyxwvutsrqponmlkjihgfedcba"
+        for letter in control_alp:
+            if cipher_alphabet.count(letter) != 1:
+                return "Wrong alphabet, some characters are special, missing or used multiple times!"
 
     for letter in text:
         if letter in alphabet:
